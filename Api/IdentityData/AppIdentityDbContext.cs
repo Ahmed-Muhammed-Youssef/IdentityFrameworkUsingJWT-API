f422e8e02828ebb422114d3247592f906b4b8d45
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-04-02 01:06:44 +0200

    Add User Database Context For EF Core
    Adds the user entity framework context and migrations.

---
+++ Api/IdentityData/AppIdentityDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Api.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.IdentityData
+{
+    // the last generic type is the type of the key
+    public class AppIdentityDbContext : IdentityDbContext<AppUser, IdentityRole<string>, string, IdentityUserClaim<string>, IdentityUserRole<string>, IdentityUserLogin<string>, IdentityRoleClaim<string>, IdentityUserToken<string>>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
