c3ab675d6513d387e4c09097fb051fdbb174228f
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-04-01 23:44:11 +0200

    Add App User Entity

---
+++ Api/Entities/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Api.Entities
+{
+    // the type determines the primary key of the users table (string <GUID> by default)
+    public class AppUser : IdentityUser<string>
+    {
+        /*
+         * Add any attributes that doesn't exist in the implemented IdentityUser.
+         * e.g an address attribute
+         */
+    }
+}
